e648924eea9a08c755948eb275f27201c0037445
simmh <simmh17@gmail.com>
2018-01-12 15:23:01 +0900

    edit blog app model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+# class Question(models.Model):
+#     question_text = models.CharField(max_length=200)
+#     pub_date = models.DateTimeField('date published')
+#
+#
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     choice_text = models.CharField(max_length=200)
+#     votes = models.IntegerField(default=0)
+
+
+class Category(models.Model):
+    name = models.CharField
+
+# class Post(models.Model):
+#     category = models.ForeignKey(Category,on_delete=models.CASCADE)
+#     title = models.CharField(max_length = 300)
+#     body = models.text
+#     created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
--- blog_project/urls.py
+++ blog_project/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog_project/', include('blog_project.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from blog import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
 ]
